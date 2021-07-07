9152a6a5b46532207c2895f1065e2c2b686daf8c
Uladzislau Melikhau <umelikhau@iba.by>
2021-07-07 10:54:35 +0300

    Exercise2

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,29 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
 public class StringDuplicatorTest {
 
+    StringDuplicator SUT;
+
+    @Before
+    public void setUp() throws Exception {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_emptyString_emptyStringReturned() {
+        String result = SUT.duplicate("");
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void duplicate_notEmptyString_duplicatedStringReturned() {
+        String result = SUT.duplicate("abc");
+        assertThat(result, is("abcabc"));
+    }
 }
